8f17fed994fa75143bb83b03b486a22f2c7707f0
Sergio Santos <sergi@Victus>
2024-08-30 10:14:12 +0100

    p4rtc_3

--- workspace/F411CEU6-4/Core/Src/main.c
+++ workspace/F411CEU6-4/Core/Src/main.c
@@ -11,97 +11,136 @@
 #include "armfunction.h"
 #include "explode.h"
 
+#define JMP_menu 100
+
 EXPLODE PA;
 
 int main(void)
 {
-  STM32FXXX_enable();
-  rtc()->inic(1); // 1 - LSE 0 - LSI (only has to be activated once)
-  PA = EXPLODE_enable();
+	STM32FXXX_enable();
+	rtc()->inic(1); // 1 - LSE 0 - LSI (only has to be activated once)
+	PA = EXPLODE_enable();
 
-  uint8_t Menu = 0;
-  uint8_t count_0 = 0;
-  uint16_t incr_0 = 0;
+	uint8_t Menu = 3;
+	uint8_t count_0 = 0;
+	uint16_t incr_0 = 0;
+	uint8_t skip_0 = 0;
 
-  gpiob()->clock(on); // lcd0
-  ARMLCD0_enable((GPIO_TypeDef*)gpiob()->instance);
-  gpioc()->clock(on); // gpioc13
-  gpioc()->instance->moder.par.pin_13 = 1;
-  gpioa()->clock(on); // inputs
-  gpioa()->instance->moder.par.pin_0 = 0;
-  gpioa()->instance->pupdr.par.pin_0 = 1;
-
-
-  FUNC_enable();
-  //HAL_Init();
-
-  char vecT[8]; // for calendar
-  PA.update(&PA.par, gpioa()->instance->idr.word.i);
-
-  while (1)
-  {
-	  /*** preamble ***/
-	  PA.update(&PA.par, gpioa()->instance->idr.word.i);
-	  /******/
-
-	  switch(Menu){
-	  case 0:
-		  lcd0()->gotoxy(0,0);
-		  lcd0()->string_size("Menu 0",7);
-		  gpioc()->instance->odr.par.pin_13 = 0;
-
-		  if(PA.par.LH & 1){
-			  incr_0 = rtc()->get_Hour();
-			  if(incr_0 > 22){ incr_0 = 0;}else{incr_0++;}
-			  rtc()->Hour(incr_0);
-		  }
-
-		  if(PA.par.LL & 1){ // Jump menu
-			  _delay_ms(500);
-			  count_0++;
-			  if(count_0 > 5){ Menu = 1; count_0 = 0;}
-		  }else{count_0 = 0;}
+	gpiob()->clock(on); // lcd0
+	ARMLCD0_enable((GPIO_TypeDef*)gpiob()->instance);
+	gpioc()->clock(on); // gpioc13
+	gpioc()->instance->moder.par.pin_13 = 1;
+	gpioa()->clock(on); // inputs
+	gpioa()->instance->moder.par.pin_0 = 0;
+	gpioa()->instance->pupdr.par.pin_0 = 1;
 
 
-		  lcd0()->gotoxy(1,0);
-		  lcd0()->string_size(func()->ui32toa(incr_0),20);
-		  break;
-	  case 1:
-		  lcd0()->gotoxy(0,0);
-		  lcd0()->string_size("Menu 1",7);
-		  gpioc()->instance->odr.par.pin_13 = 1;
+	FUNC_enable();
+	//HAL_Init();
 
+	char vecT[8]; // for calendar
+	PA.update(&PA.par, gpioa()->instance->idr.word.i);
 
+	while (1)
+	{
+		/*** preamble ***/
+		PA.update(&PA.par, gpioa()->instance->idr.word.i);
+		/******/
 
+		switch(Menu){
+		case 0:
+			lcd0()->gotoxy(0,0);
+			lcd0()->string_size("Set Hour",10);
+			gpioc()->instance->odr.par.pin_13 = 0;
 
+			if(PA.par.LH & 1){
+				if(skip_0 > 0){
+					incr_0 = rtc()->get_Hour();
+					if(incr_0 > 22){ incr_0 = 0;}else{incr_0++;}
+					rtc()->Hour(incr_0);
+				}
+				skip_0++;
+			}
 
+			if(PA.par.LL & 1){ // Jump menu
+				_delay_ms(JMP_menu);
+				count_0++;
+				if(count_0 > 5){ Menu = 1; count_0 = 0; skip_0 = 0;}
+			}else{count_0 = 0;}
 
-		  if(PA.par.LL & 1){ // Jump menu
-		  	_delay_ms(500);
-		  	count_0++;
-		  	if(count_0 > 5){ Menu = 0; count_0 = 0;}
-		  }else{count_0 = 0;}
-		  break;
-	  default:
-		  break;
-	  }
+			break;
+		case 1:
+			lcd0()->gotoxy(0,0);
+			lcd0()->string_size("Set Minute",10);
+			gpioc()->instance->odr.par.pin_13 = 1;
 
-	  lcd0()->gotoxy(3,0);
-	  rtc()->tr2vec(vecT);
-	  lcd0()->string_size(func()->print_v2("hora: %d%d:%d%d:%d%d", vecT[0],vecT[1],vecT[2],vecT[3],vecT[4],vecT[5]),14);
+			if(PA.par.LH & 1){
+				if(skip_0 > 0){
+					incr_0 = rtc()->get_Minute();
+					if(incr_0 > 58){ incr_0 = 0;}else{incr_0++;}
+					rtc()->Minute(incr_0);
+				}
+				skip_0++;
+			}
 
+			if(PA.par.LL & 1){ // Jump menu
+				_delay_ms(JMP_menu);
+				count_0++;
+				if(count_0 > 5){ Menu = 2; count_0 = 0; skip_0 = 0;}
+			}else{count_0 = 0;}
 
+			break;
+		case 2:
+			lcd0()->gotoxy(0,0);
+			lcd0()->string_size("Set Second",10);
+			gpioc()->instance->odr.par.pin_13 = 0;
 
-  } /*** While ***/
+			if(PA.par.LH & 1){
+				if(skip_0 > 0){
+					incr_0 = rtc()->get_Second();
+					if(incr_0 > 59){ incr_0 = 0;}else{incr_0++;}
+					rtc()->Second(incr_0);
+				}
+				skip_0++;
+			}
+
+			if(PA.par.LL & 1){ // Jump menu
+				_delay_ms(JMP_menu);
+				count_0++;
+				if(count_0 > 5){ Menu = 3; count_0 = 0; skip_0 = 0;}
+			}else{count_0 = 0;}
+
+			break;
+		case 3:
+			lcd0()->gotoxy(0,0);
+			lcd0()->string_size("Clock",10);
+			gpioc()->instance->odr.par.pin_13 = 1;
+
+			if(PA.par.LL & 1){ // Jump menu
+				_delay_ms(JMP_menu);
+				count_0++;
+				if(count_0 > 5){ Menu = 0; count_0 = 0; skip_0 = 0;}
+			}else{count_0 = 0;}
+
+			break;
+		default:
+			break;
+		}
+
+		lcd0()->gotoxy(3,0);
+		rtc()->tr2vec(vecT);
+		lcd0()->string_size(func()->print_v2("hora: %d%d:%d%d:%d%d", vecT[0],vecT[1],vecT[2],vecT[3],vecT[4],vecT[5]),14);
+
+	} /*** While ***/
 } /*** Main ***/
 
 
 void Error_Handler(void)
 {
-  __disable_irq();
-  while (1)
-  {
-  }
+	__disable_irq();
+	while (1)
+	{
+	}
 }
 
 #ifdef  USE_FULL_ASSERT
