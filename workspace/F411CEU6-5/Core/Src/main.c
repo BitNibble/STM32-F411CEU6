d6fb7909e52eb3af9095f6b5660385e4571fcf33
Sergio Santos <sergi@Victus>
2024-09-14 16:20:07 +0100

    411prf_5_3

--- workspace/F411CEU6-5/Core/Src/main.c
+++ workspace/F411CEU6-5/Core/Src/main.c
@@ -22,8 +22,14 @@
 
 EXPLODE PA;
 
+void ADC_setup(void);
+
 int main(void)
 {
+	uint8_t tmp;
+	uint16_t adc1_dr;
+	uint32_t time_out;
+	uint32_t count_0 = 0;
 	STM32FXXX_enable();
 	//rtc()->inic(1); // 1 - LSE 0 - LSI (only has to be activated once)
 	PA = EXPLODE_enable();
@@ -41,19 +47,39 @@ int main(void)
 	//HAL_Init();
 
 	PA.update(&PA.par, gpioa()->instance->idr.word.i);
+	ADC_setup();
 
 	while (1)
 	{
 		/*** preamble ***/
 		PA.update(&PA.par, gpioa()->instance->idr.word.i);
 		/******/
+		adc1_dr = stm()->adc1->instance->dr.reg;
+		stm()->adc1->instance->cr2.par.swstart = on;
+		for(time_out = 2000000, tmp = 1; tmp && time_out; tmp = stm()->adc1->instance->cr2.par.swstart, time_out-- );
+
 		lcd0()->gotoxy(0,0);
 		lcd0()->string_size("Hello!",10);
-
-
-
-
+		lcd0()->gotoxy(1,0);
+		lcd0()->string_size(func()->ui16toa(adc1_dr),10);
+		lcd0()->gotoxy(2,0);
+		lcd0()->string_size(func()->ui32toa(count_0++),10);
 	}
+}
+
+void ADC_setup(void){
+	/********** ADC1 **********/
+	stm()->adc1->iclock(on);
+	stm()->adc1->clock(on);
+	stm()->adc1->common_instance->ccr.par.adcpre = 3;
+	stm()->adc1->instance->smpr1.par.smp18 = 7;
+	stm()->adc1->instance->sqr1.par.l = 0;
+	stm()->adc1->instance->sqr3.par.sq1 = 18;
+	stm()->adc1->common_instance->ccr.par.tsvrefe = on;
+	//stm()->adc1->common_instance->ccr.par.vbate = on;
+	stm()->adc1->instance->cr2.par.eocs = on;
+	stm()->adc1->instance->cr2.par.dds = on;
+	stm()->adc1->instance->cr2.par.adon = on;
 }
 
 void Error_Handler(void)
